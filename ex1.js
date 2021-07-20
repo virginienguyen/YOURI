a1e7d13ab2a49bb8078a38ca44c39b746c5fff7d
virginienguyen <virginie.nguyen@vianh.fr>
2021-07-20 14:15:50 +0200

    double click

--- ex1.js
+++ ex1.js
@@ -1,25 +1,7 @@
-// Get the modal
-var modal = document.getElementById('myModal');
- 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    modalImg.alt = this.alt;
-    captionText.innerHTML = this.alt;
+function zoomzoom() {
+    let imagedog = document.getElementById("laphotoduchien")
+    let taille = imagedog.clientWidth;
+    if (taille <=2000){
+    imagedog.style.width=taille + 100 + "px";
+    }
 }
- 
- 
-// When the user clicks on <span> (x), close the modal
-modal.onclick = function() {
-    img01.className += " out";
-    setTimeout(function() {
-       modal.style.display = "none";
-       img01.className = "modal-content";
-     }, 400);
-    
- }    
-    

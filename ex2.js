bf1e8f05c8d324a5a5f226f763baaa20959945a3
virginienguyen <virginie.nguyen@vianh.fr>
2021-07-20 16:20:48 +0200

    code ok

---
+++ ex2.js
@@ -0,0 +1,6 @@
+function changePic1() {
+    document.getElementById("myImg").src = "src/casse.jpg";
+       }
+      function changePic2(){
+       document.getElementById("myImg").src = "src/clean.jpg";
+       }

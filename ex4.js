cd7c6973a88aaded9f44d9b0ffbbc1cebcdf7b3c
virginienguyen <virginie.nguyen@vianh.fr>
2021-07-21 12:36:56 +0200

    code ok

--- ex4.js
+++ ex4.js
@@ -1,3 +1,7 @@
-let Border = object.getElementById("Border");
-Border.focus = () => object.style.border = "thin dotted red";
-Border.focusout = () => object.style.border = "green";
+document.getElementById("bour").addEventListener("focus",function() {
+    this.style.border = "5px solid green";
+    this.style.outline ="none";
+  });
+  document.getElementById("bour").addEventListener("focusout",function() {
+    this.style.border = "5px solid red";
+  });
